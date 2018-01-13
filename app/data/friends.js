3f5ab0b65db892b18ede87acc7a457ab42b000cf
Robyn Harka <reharka@gmail.com>
2018-01-13 03:27:04 -0500

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+    {
+        name: 'Frank',
+        photo: 'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/German-Shepherd-Dog-1.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Lucy',
+        photo: 'https://yt3.ggpht.com/EdjnobpzppDl5pSVU2s2AUIiFS0qBfT8Jdodw-FHMhugJK5zmzWDLkpqDVtpnaLSP66M5F8nqINImLKGtQ=s900-mo-c-c0xffffffff-rj-k-no',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Bob',
+        photo: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Natural-Dog-Law-2-To-dogs%2C-energy-is-everything.jpg?itok=Z-ujUOUr',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Chuy',
+        photo: 'https://i.pinimg.com/736x/02/d7/01/02d701b77a984a1b6cf970e6eb0468e1--teacup-maltipoo-maltipoo-puppies.jpg',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Cesar',
+        photo: 'https://shibashake.com/wp-content/blogs.dir/7/files/2010/03/IMG_1239-520x390.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Cloe',
+        photo: 'http://cdn1-www.dogtime.com/assets/uploads/gallery/brittany-dogs-and-puppies/brittany-spaniel-dogs-puppies-9.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Dave',
+        photo: 'https://i.pinimg.com/736x/94/d3/d9/94d3d9f62c08d142c99f07c79c0e55f6--dogs-and-puppies-cutest-puppies-puppies.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Joey Jr',
+        photo: 'https://i.ebayimg.com/00/s/MTA2N1gxNjAw/z/yU0AAOSwUYNZ8XEw/$_75.JPG',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Stella',
+        photo: 'http://cdn2-www.dogtime.com/assets/uploads/gallery/alaskan-malamute-dogs-and-puppies/alaskan-malamute-dogs-puppies-8.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Zoey',
+        photo: 'https://i.pinimg.com/736x/12/27/6e/12276e1e10fd61b8e117a204922f2d83--baby-beagle-beagle-puppies.jpg',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
